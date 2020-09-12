69eac3ebc37711f03a13e4e5e40ec5fd3cfc878f
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-12 07:29:17 +0530

    Type Casting

---
+++ Assignment 1/typeCast.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String args[]) {
+        //a
+        byte a = 10;
+        a = (byte)(a+10);
+        System.out.println(a);
+        //b
+        short b = 15;
+        int c = (int)(b/3.0);
+        System.out.println(c);
+        //c
+        double A = 12.9;
+        float B = (float)(a/3.5);
+        System.out.println(B);
+    }
+}
+
