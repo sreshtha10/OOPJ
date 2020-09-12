33f1aead3a8cd162e557af14ab51d8f2c1c8f38e
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-12 07:57:33 +0530

    Update GradeIfElseladder.java

--- Assignment 1/GradeIfElseladder.java
+++ Assignment 1/GradeIfElseladder.java
@@ -7,19 +7,19 @@ class Main {
        if(marks > 90){
            System.out.println("Grade A+");
        }
-       else if(marks < 90 && marks >80){
+       else if(marks >80){
            System.out.println("Grade A");
        }
-       else if(marks <80 && marks >70){
+       else if(marks >70){
            System.out.println("Grade B");
        }
-       else if(marks <70 && marks >60){
+       else if(marks >60){
            System.out.println("Grade C");
        }
-       else if(marks <60 && marks >50){
+       else if(marks >50){
            System.out.println("Grade D");
        }
-       else if(marks <50 && marks >40){
+       else if(marks >40){
            System.out.println("Grade E");
        }
        else{
