11dccb0807bf33b4b3ef574165503912080e24c9
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-26 15:51:51 +0530

    Update 1.java

--- Assignment 2/1.java
+++ Assignment 2/1.java
@@ -1,1 +1,16 @@
-// 1
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array:\t");
+        int arr[] = new int[10];
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Elements of the array:\t");
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
