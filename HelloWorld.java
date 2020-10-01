0352161f85ee4e5242df9a3611354ebeb260f414
Ayush Kumar Singh <57259393+aksse99@users.noreply.github.com>
2020-10-01 12:38:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+/*
+In java we have many ways to print a single line
+Most commonly used are given bellow :-
+*/
 class Main{
     public static void main(String[] agrs){
-        System.out.println("Hello World");
+        System.out.print(" Hello World 1");
+        System.out.println("Hello world 2");
+        System.out.printf("Hello world 3");
     }
 }
