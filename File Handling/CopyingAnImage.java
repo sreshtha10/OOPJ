bf189c08aeb7cc4fe5ba05aa16bc61f5669a9d32
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-14 21:34:04 +0530

    Update CopyingAnImage.java

--- File Handling/CopyingAnImage.java
+++ File Handling/CopyingAnImage.java
@@ -4,17 +4,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 public class Main{
 	public static void main(String args[]) throws IOException{
-		File newFile = new File("C:\\Users\\Sreshtha\\Desktop\\File Handling Java\\batman.jpg");
+		File newFile = new File("batman.jpg");
 		FileInputStream inputStream = new FileInputStream(newFile);
 		// read from the input stream
 		byte bytes[] = new byte[(int)newFile.length()];
 		int numOfBytes = inputStream.read(bytes); // copying the data
 		try { //If the file already exist
-			FileOutputStream outputStream = new FileOutputStream("C:\\Users\\Sreshtha\\Desktop\\File Handling Java\\superman.jpg");
+			FileOutputStream outputStream = new FileOutputStream("superman.jpg");
 			outputStream.write(bytes);
 		}
 		catch (IOException e) {
-			File f1 = new  File("C:\\Users\\Sreshtha\\Desktop\\File Handling Java\\superman.jpg");
+			File f1 = new  File("superman.jpg");
 			FileOutputStream outputStream = new FileOutputStream(f1);
 			outputStream.write(bytes);
 		}
