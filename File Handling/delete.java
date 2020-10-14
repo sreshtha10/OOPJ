4ce464aea45e0efd568fc963a396e40454971c00
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-14 20:03:11 +0530

    Deleting a file using file handling

---
+++ File Handling/delete.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main{
+	public static void main(String args[]) {
+		try {  
+			File myFile = new File("details.txt");
+			if(myFile.exists()) {
+				if(myFile.delete()) {
+					System.out.println("File Deleted: "+myFile.getName());
+				}
+				else {
+					System.out.println("Failed to delete the file");
+				}
+			}
+			else {
+				System.out.println("File doesn't exist.");
+			}
+		}
+		catch (Exception  e) {
+			System.out.println("Error occured");
+			System.exit(1);
+		}
+	}
+}
