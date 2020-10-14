81bee3f7b7ceb0d882031d92c52dc2e24bc7f3cc
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-14 19:35:07 +0530

    Writing in a file

---
+++ File Handling/WritingToFile.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class Main{
+	public static void main(String args[]) {
+		try {
+			File myFile = new File("C:\\Users\\Sreshtha\\Desktop\\File Handling Java\\details.txt");
+			if(myFile.createNewFile()) {
+				System.out.println("File created: "+myFile.getName());
+			}
+			else {
+				System.out.println("File Already Exists");
+			}
+			FileWriter myWriter = new FileWriter(myFile);
+			myWriter.write("Hello World");
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		}
+		catch (IOException e) {
+			System.out.println("Error occured");
+			System.exit(1);
+		}
+	}
+}
