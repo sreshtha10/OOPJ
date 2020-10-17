33c4c9bec91a8a5e67b044d766715d761b65e49e
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-17 16:47:25 +0530

    my codechef template

---
+++ MycodechefTemplate.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws IOException
+	{
+	    try{
+	    	InputStreamReader r = new InputStreamReader(System.in);
+	    	BufferedReader reader = new BufferedReader(r);
+	    	int test = Integer.parseInt(reader.readLine());
+	    	while(test !=0) {
+	    		
+	    	}
+
+	    	
+	    }
+	    catch(Exception e){
+	        return;
+	    }
+	}
+}
