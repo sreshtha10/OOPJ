399fd3ed8e8cc5c3b6ab653b5f450c02a82e2c9a
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-17 16:53:08 +0530

    Create 1.java

---
+++ Assignment 3/1.java
@@ -0,0 +1,13 @@
+class Box{
+	double height,width,depth;
+	Box(double height , double width,double depth){
+		this.height = height;
+		this.width = width;
+		this.depth = depth;
+	}
+}
+class Main{
+	public static void main(String args[]) {
+		Box box1 = new Box(12,13,14);
+	}
+}
